fb751e574afadd5d14a0a59d66bed4f58957671d
Siddhant Jaiswal <siddhantjaiswal363@gmail.com>
2024-02-23 15:29:58 +0530

    Path variable

--- springboot-rest-api/springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ springboot-rest-api/springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -2,7 +2,9 @@ package net.javaguides.springboot.controller;
 
 import net.javaguides.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
 
 @RestController
 public class StudentController {
@@ -12,7 +14,32 @@ public class StudentController {
     @GetMapping("student")
     public Student getStudent()
     {
-        Student student = new Student(1, "Siddhant", "Jaiswal");
-        return student;
+        return new Student(1, "Siddhant", "Jaiswal");
+    }
+
+
+
+    //Making a REST API which will return a list of objects
+
+    @GetMapping("students")
+    public List<Student> getStudents()
+    {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Siddhant", "Jaiswal"));
+        students.add(new Student(2, "ABC", "XYZ"));
+        students.add(new Student(3, "KLM", "NOP"));
+
+        return  students;
+    }
+
+    //SpringBoot REST API with Path Variable
+    //{id} - URI template variable
+    //http://localhost:8080/student/1
+
+    @GetMapping("students/{id}")
+    public Student StudentPathVariable(@PathVariable("id") int studentID)
+    {
+
+        return new Student(studentID, "SIDDHANT ID ", "JAISWAL ID ");
     }
 }
