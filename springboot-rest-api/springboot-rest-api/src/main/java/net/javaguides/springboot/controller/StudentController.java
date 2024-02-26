3f705f37d9ef0e97f421d14e9b21b16abe0bc0d3
Siddhant Jaiswal <siddhantjaiswal363@gmail.com>
2024-02-26 17:03:14 +0530

    updating with REST API PUT Request

--- springboot-rest-api/springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ springboot-rest-api/springboot-rest-api/src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -86,5 +86,16 @@ public class StudentController {
         return  student;
     }
 
+    //Spring Boot rest api that handles HTTP PUT request - updating existing resource
+//    http://localhost:8080/students/1/update
+    @PutMapping("students/{id}/update")
+
+    public Student updateStudent(@RequestBody Student student, @PathVariable("id") int studentId)
+    {
+            System.out.println(student.getFirstName());
+            System.out.println(student.getLastName());
+            return  student;
+    }
+
 
 }
