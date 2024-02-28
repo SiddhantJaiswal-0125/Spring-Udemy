0e2f46afda438470d32ff46e031be23af24a8428
Siddhant Jaiswal <siddhantjaiswal363@gmail.com>
2024-02-29 00:01:32 +0530

    created a table in the DB through Spring Boot

--- springboot-restful-webservices/springboot-restful-webservices/src/main/java/net/javaguides/springboot/entity/User.java
+++ springboot-restful-webservices/springboot-restful-webservices/src/main/java/net/javaguides/springboot/entity/User.java
@@ -1,20 +1,29 @@
 package net.javaguides.springboot.entity;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 
+//JPA ANNOTATION
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column( nullable = false)
     private String firstName;
+
+    @Column(nullable = false)
     private String lastName;
+
+    @Column(nullable = false,unique = true)
     private String email;
 
 }
