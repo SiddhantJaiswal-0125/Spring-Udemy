6ed7f25bd84ca1f123b9c1be9bd1e1876475e314
Siddhant Jaiswal <siddhantjaiswal363@gmail.com>
2024-02-28 23:57:19 +0530

    Added lombok annotator

---
+++ springboot-restful-webservices/springboot-restful-webservices/src/main/java/net/javaguides/springboot/entity/User.java
@@ -0,0 +1,20 @@
+package net.javaguides.springboot.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
