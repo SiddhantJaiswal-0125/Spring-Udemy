0f399b949b752098222f5c2d3aa0a36d2bbc044e
Siddhant Jaiswal <siddhantjaiswal363@gmail.com>
2024-03-05 17:10:20 +0530

    update

--- todo-management/todo-management/src/main/java/net/javaguides/todo/service/impl/TodoServiceImpl.java
+++ todo-management/todo-management/src/main/java/net/javaguides/todo/service/impl/TodoServiceImpl.java
@@ -3,8 +3,10 @@ package net.javaguides.todo.service.impl;
 import lombok.AllArgsConstructor;
 import net.javaguides.todo.dto.TodoDto;
 import net.javaguides.todo.entity.Todo;
+import net.javaguides.todo.exception.ResourceNotFoundException;
 import net.javaguides.todo.repository.TodoRepository;
 import net.javaguides.todo.service.TodoService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 
@@ -12,26 +14,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class TodoServiceImpl implements TodoService {
     private TodoRepository todoRepository;
+    private ModelMapper modelMapper;
 
     @Override
     public TodoDto addTodo(TodoDto todoDto) {
 
         //Convert TodoDto into Todo JPA Entity;
-
-        Todo todo = new Todo();
-        todo.setTitle(todoDto.getTitle());
-        todo.setDescription(todoDto.getDescription());
-        todo.setCompleted(todoDto.getCompleted());
-
+        Todo todo = modelMapper.map(todoDto, Todo.class);
         Todo savedTodo = todoRepository.save(todo);
 
         //convert SavedTodo to TodoDTO Entity;
-        TodoDto savedTodoDto = new TodoDto();
-        savedTodoDto.setId(savedTodo.getId());
-        savedTodoDto.setTitle(savedTodo.getTitle());
-        savedTodoDto.setCompleted(savedTodo.isCompleted());
-        savedTodoDto.setDescription(savedTodo.getDescription());
-
+        TodoDto savedTodoDto = modelMapper.map(savedTodo, TodoDto.class);
         return savedTodoDto;
+    }
+
+    @Override
+    public TodoDto getTodo(Long id) {
+        Todo todo = todoRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Todo Not found by id "+id));
+        return modelMapper.map(todo, TodoDto.class);
     }
 }
